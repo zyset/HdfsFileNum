b5cdbb57e4dd3521fe7428c8d2b795e55eb7af4a
TimZhang <1581404769@qq.com>
2018-08-23 14:27:56 +0800

    init project

---
+++ src/main/java/tim/HdfsFileNum/App.java
@@ -0,0 +1,13 @@
+package tim.HdfsFileNum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/tim/HdfsFileNum/StartApp.java
@@ -0,0 +1,16 @@
+package tim.HdfsFileNum;
+
+public class StartApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int fileFilterSize = 1;
+		String byte_type = "mb";  //b;kb;mb;gb;tb
+		String hdfs_path = "hdfs://22.224.64.99:8020/data/"; //hdfs://22.224.64.99:8020/data/  hdfs://22.224.64.99:8020/dbp_data_bak  hdfs://22.224.64.99:8020/apps/hive/warehouse
+		String fileName = "data";
+		long fileNum = 1000;
+		
+		
+	}
+
+}
